10f6088d5ad2b10aea287b1d856e3efe56bda8ab
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-20 22:45:12 -0300

    feat: criated style files

---
+++ movies/src/assets/styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+    primaryColor:'#267365',
+    secundaryColor: '#F29F05',
+
+    baseBg1: '#E4F2E7',
+    baseBg2: '#97A6A0',
+    baseBg3: '#2D3E40',
+
+    colorText1: '#14140F',
+    colorText2: '#FFFFFF',
+}
